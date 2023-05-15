7ff18e8c32ccad8356cad52487d0be53c38034dc
Lorenzo <lo.peixoto@gmail.com>
2023-05-15 14:36:33 -0300

    Construtor da classe Controle

--- src/Controle.java
+++ src/Controle.java
@@ -4,4 +4,11 @@ public class Controle {
     private ArrayList <Jogador> jogadores;
     private Jogador atual;
     private String correta;
+
+    public Controle() {
+        this.correta = "";
+        this.jogadores = new ArrayList<>();
+        recordista();
+        this.atual = bemVindo();
+    }
 }
